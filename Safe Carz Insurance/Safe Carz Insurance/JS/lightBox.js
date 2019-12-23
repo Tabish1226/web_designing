51cb9248345fed73066a65d104261832f0a671c9
Tabish1226 <Tabish1226@users.noreply.github.com>
2019-12-24 02:22:43 +0500

    Add files via upload

---
+++ Safe Carz Insurance/Safe Carz Insurance/JS/lightBox.js
@@ -0,0 +1,39 @@
+// JavaScript Document Light Box
+function lightBox1(){
+		var lb1 = document.getElementById("lightBox1");
+		var lb1Bg = document.getElementById("lightBoxBg");
+		
+		lb1.style.display='block';
+		lb1Bg.style.display='block';
+	}
+function lightBox2(){
+		var lb2 = document.getElementById("lightBox2");
+		var lb2Bg = document.getElementById("lightBoxBg");
+		
+		lb2.style.display='block';
+		lb2Bg.style.display='block';
+	}
+function lightBox3(){
+		var lb3 = document.getElementById("lightBox3");
+		var lb3Bg = document.getElementById("lightBoxBg");
+		
+		lb3.style.display='block';
+		lb3Bg.style.display='block';
+	}		
+function dismiss(){
+		var lb1 = document.getElementById("lightBox1");
+		var lb1Bg = document.getElementById("lightBoxBg");
+		var lb2 = document.getElementById("lightBox2");
+		var lb2Bg = document.getElementById("lightBoxBg");
+		var lb3 = document.getElementById("lightBox3");
+		var lb3Bg = document.getElementById("lightBoxBg");
+	
+		
+		
+		lb1.style.display='none';
+		lb1Bg.style.display='none';
+		lb2.style.display='none';
+		lb2Bg.style.display='none';		
+		lb3.style.display='none';
+		lb3Bg.style.display='none';	
+	}	
